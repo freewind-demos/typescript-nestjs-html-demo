5988092af93e8c9d87c2d4da2c27d7cc74dd7369
Freewind <nowindlee@gmail.com>
2024-01-30 21:53:25 +0800

    find-port

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,10 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
-    // NOTE return data will be used in public/index.html if possible
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import portfinder from 'portfinder';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  
-  await app.listen(3000);
-  console.log("open http://localhost:3000")
+
+  await app.listen(await portfinder.getPortPromise());
+  console.log(await app.getUrl())
 }
 bootstrap();
